3fa12a6194de4b613b94337c0863bc0ee59fd2d3
sarabizjak7 <sarabizjak97@gmail.com>
2020-12-07 22:12:02 +0100

    HW2 added.

--- HW/TDA_HW2_sarabizjak/generate.py
+++
@@ -1,7 +0,0 @@
-from delauney import *
-from linesweeptriangulation import *
-
-S = [(random.uniform(0, 20), random.uniform(0, 20), random.uniform(0, 20)) for _ in range(100)]
-
-_, T = triangulate(S)
-optimize(T)
